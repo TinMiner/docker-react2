19450ee9fdedd554e891c8cbd2e820a1c58e94c6
Alan Beecroft <alan.beecroft@talk21.com>
2021-01-24 12:59:42 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Che cosa fai stasera?
+          Ci vediamo.
         </p>
        
           E piove!
